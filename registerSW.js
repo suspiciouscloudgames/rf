bf03742ab3212925b5ca2860a31071da0ecb94f9
suspiciouscloudgames <suspiciouscloud@gmail.com>
2026-08-28 00:29:54 +0900

    Deploy observation copy and subtitle styling

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/rf/sw.js', { scope: '/rf/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
